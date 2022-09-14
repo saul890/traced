df4c1d28d14ce367e0837f2eda2f3b6d9fc642c2
Saul Hirner <saulhirner@gmail.com>
2022-09-14 13:22:35 -0400

    functioning

--- server.js
+++ server.js
@@ -1,7 +1,42 @@
-let sound = document.getElementById('link')
+let sound = document.getElementsByClassName('link')
 
-audio = new Audio('audio/audio.mp3');
+audioOne = new Audio('audio/library.mp3');
+audioTwo = new Audio('audio/bars.mp3');
+audioThree = new Audio('audio/ocean.mp3');
+audioFour = new Audio('audio/bars.mp3');
+audioFive = new Audio('audio/snow.mp3');
+audioSix = new Audio('audio/busycar.mp3');
+audioSeven = new Audio('audio/nature.mp3');
+audioEight = new Audio('audio/data.mp3');
 
-function playSound() {
-    audio.play();
-  }
+function playSoundOne() {
+  audioOne.play();
+}
+
+function playSoundTwo() {
+  audioTwo.play();
+}
+
+function playSoundThree() {
+  audioThree.play();
+}
+
+function playSoundFour() {
+  audioFour.play();
+}
+
+function playSoundFive() {
+  audioFive.play();
+}
+
+function playSoundSix() {
+  audioSix.play();
+}
+
+function playSoundSeven() {
+  audioSeven.play();
+}
+
+function playSoundEight() {
+  audioEight.play();
+}
